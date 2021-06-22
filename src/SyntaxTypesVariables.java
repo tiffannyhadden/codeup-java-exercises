239efe9b36e06c26a12f47b974ae2bcbbc044a8d
tiffannyhadden <tiffanny.hadden@gmail.com>
2021-06-21 17:29:14 -0700

    adds syntax

---
+++ src/SyntaxTypesVariables.java
@@ -0,0 +1,26 @@
+//public class SyntaxTypesVariables {
+//
+//    public static void main(String[] args) {
+//
+////            byte myFirstNumber = 127;
+////        System.out.println(myFirstNumber + 1);
+//
+////        System.out.println('H' + 'e' + 'l');
+////        //short
+////        //int
+////                System.out.println((int)'h');
+////                //short
+////                //int
+//        //every character has a value
+////        byte myChar = 'd'; //100
+////        myChar += 'A'; //65
+////        System.out.println(myChar);
+//
+////        String myName = "Tiffanny";
+////        System.out.println(myName.toUpperCase());
+//
+////        String myName = "Tiffanny";
+////        System.out.println(myName.toUpperCase());
+////
+////    }
+////}
