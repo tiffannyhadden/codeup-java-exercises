446306ed61f39e3c625ae2a2576a750433ac5788
tiffannyhadden <tiffanny.hadden@gmail.com>
2021-06-22 09:45:40 -0700

    adds #14

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,61 @@
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello, world!");
+        int myFavoriteNumber = 22;
+        System.out.println(myFavoriteNumber);
+//        String myString = "Tiffanny";
+//       System.out.println(myString);
+//       char myString = 'T';
+//        System.out.println(myString);
+        String myString = "3.14159";
+        System.out.println(myString);
+//        long myNumber;
+//        System.out.println(myNumber);
+//        float myNumber = 3.14f;
+//        System.out.println(myNumber);
+//        long myNumber = 123L;
+//        System.out.println(myNumber);
+//        float myNumber = 123f;
+//        System.out.println(myNumber);
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//        class
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;// it no likey
+//        int three = (int) "three"; //cannot cast java.lang.string
+//        int x = 4;
+////        x = x + 5;
+//         x +=5;
+//        System.out.println(x);
+//        int x = 3;
+//        int y = 4;
+////        y = y * x;
+//        y *= x;
+//        System.out.println(y);
+//        int x = 10;
+//        int y = 2;
+////        x = x / y;
+////        y = y - x;
+//        x /= y;
+//        System.out.println(x);
+//        y -= x;
+//        System.out.println(y);
+
+        int x = Integer.MIN_VALUE;
+        System.out.println(--x);
+
+
+
+
+
+
+
     }
 }
+
+
