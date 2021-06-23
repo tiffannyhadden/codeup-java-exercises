2295646b04f8a4d2afd035bc72f970b28b98bf43
tiffannyhadden <tiffanny.hadden@gmail.com>
2021-06-23 12:56:10 -0500

    adds laura lexture

---
+++ src/LauraLexture.java
@@ -0,0 +1,60 @@
+//public class LauraLexture {
+//    public static void main(String[] args) {
+//   String myFavAnimal = "frog";
+//        System.out.println(myFavAnimal);
+//
+//        int chanceOfRain = 60;
+//        if(chanceOfRain >= 50){
+//            System.out.println("Stay inside");
+//        }
+//
+//        boolean isWarm = true;
+//        boolean isSunny = false;
+//        if(isWarm && isSunny){
+//            System.out.println("go swimming");
+
+            //&& will stop evaluating if the first condition is false
+            //& will still read both conditions no matter what
+
+            //if(isWarm || isSunny){
+            // System.out.println("go swimming");
+
+            //if(isWarm | isSunny){
+            // System.out.println("2nd go swim");
+
+            //String myName = "Laura";
+            //String yourName = "Laura";
+            //.equals()
+            //System.out.println(myName.equals(yourName));
+            //.equalsIgnoreCase
+            //System.out.println(myName.equalsIgnoreCase(yourName));
+
+            //CONTROL STRUCTURES
+
+            //if(/*condition*/){
+            //execute code if condition is met to be true
+
+            //        MINI EXERCISE
+            // TODO: create three unique usernames (user1, user2, user3).
+            //  create a fourth user and set it equal to user input. (ask
+            //  the user to enter a username). if the username has already
+            //  been used by user1, user2, or user3, print "sorry this
+            //  username already exists.
+
+//            scanner sc = new Scanner(System.in);
+//
+//            String user1 = "demo";
+//            String user2 = "prachi";
+//            String user3 = "josh";
+//
+//            System.out.println("Please enter a username");
+//
+//            String user4 = sc.nextLine();
+//            if(user4.equalsIgnoreCase(user1) || user4.equalsIgnoreCase(user2) || user4.equalsIgnoreCase(user3) {
+//                System.out.println("sorry this username already exists");
+////            }
+////
+////        }
+//
+//    }
+//}
