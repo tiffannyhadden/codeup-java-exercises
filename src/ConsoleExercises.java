b659fc47145b5635a5920cc16694466e81c7f389
tiffannyhadden <tiffanny.hadden@gmail.com>
2021-06-23 11:38:20 -0500

    adds changes after walkthrough

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -4,6 +4,9 @@ public class ConsoleExercises {
     public static void main(String[] args) {
         double pi = 3.14159;
         System.out.format("The value of pi is approximately %s", pi);
+        //caseys version of above
+        //System.out.printf("The value of pi is approximately %.2f.", pi);
+
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter an integer");
         int myNum = sc.nextInt();
@@ -22,6 +25,16 @@ public class ConsoleExercises {
             int area = length + width;
             System.out.printf("The area of the room is %d", area);
 
+            //caseys
+//            int length = Integer.parseInt(sc.nextLine());
+//            int width = Integer.parseInt(sc.nextLine());
+
+            System.out.printf("Perimeter: %s\n", (2 * length) + (2 * width));
+            System.out.printf("Area: %s", length * width);
+
+            //bonus
+            // float length = Float.parseFloat(sc.nextLine());
+            // float width = Float.parseFloat(sc.nextLine());
 
 
 
