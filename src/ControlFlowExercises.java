7f1b85c4be85c47a69a5104ec42ad5d905e3355c
tiffannyhadden <tiffanny.hadden@gmail.com>
2021-06-28 01:58:28 -0500

    adds do while

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -8,10 +8,41 @@ public class ControlFlowExercises {
         //Your output should look like this:
         //
         //5 6 7 8 9 10 11 12 13 14 15
-        int i = 5;
-        while (i <= 15) {
-            System.out.println( + i);
-            i++;
-        }
+//        int i = 5;
+//        while (i <= 15) {
+//            System.out.println( + i);
+////            i++;
+//        }
+        //Do While
+        //
+        //Create a do-while loop that will count by 2's starting with 0 and ending at 100. Follow each number with a new line.
+        //Alter your loop to count backwards by 5's from 100 to -10.
+        //Create a do-while loop that starts at 2, and displays the number squared on each line while the number is less than 1,000,000. Output should equal:
+        //
+        // 2
+        // 4
+        // 16
+        // 256
+        // 65536
+//        int i = 0;
+////        do {
+////            System.out.println(i);
+////            i+=2;
+////        } while (i <= 100);
+//
+////        int i = 100;
+////        do {
+////            System.out.println(i);
+////            i-=5;
+////        } while (i >= -10);
+
+//        int i=2;
+//        do{
+//
+//                System.out.println(i);
+//            i*=i;
+//        }while(i < 1000);
+//
     }
 }
+
