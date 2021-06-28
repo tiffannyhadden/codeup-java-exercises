416622a55566f170887714f783d74dbea89985e6
tiffannyhadden <tiffanny.hadden@gmail.com>
2021-06-27 20:40:08 -0500

    adds to laura lexture

--- src/LauraLexture.java
+++ src/LauraLexture.java
@@ -1,5 +1,5 @@
-//public class LauraLexture {
-//    public static void main(String[] args) {
+public class LauraLexture {
+    public static void main(String[] args) {
 //   String myFavAnimal = "frog";
 //        System.out.println(myFavAnimal);
 //
@@ -13,33 +13,33 @@
 //        if(isWarm && isSunny){
 //            System.out.println("go swimming");
 
-            //&& will stop evaluating if the first condition is false
-            //& will still read both conditions no matter what
+        //&& will stop evaluating if the first condition is false
+        //& will still read both conditions no matter what
 
-            //if(isWarm || isSunny){
-            // System.out.println("go swimming");
+        //if(isWarm || isSunny){
+        // System.out.println("go swimming");
 
-            //if(isWarm | isSunny){
-            // System.out.println("2nd go swim");
+        //if(isWarm | isSunny){
+        // System.out.println("2nd go swim");
 
-            //String myName = "Laura";
-            //String yourName = "Laura";
-            //.equals()
-            //System.out.println(myName.equals(yourName));
-            //.equalsIgnoreCase
-            //System.out.println(myName.equalsIgnoreCase(yourName));
+        //String myName = "Laura";
+        //String yourName = "Laura";
+        //.equals()
+        //System.out.println(myName.equals(yourName));
+        //.equalsIgnoreCase
+        //System.out.println(myName.equalsIgnoreCase(yourName));
 
-            //CONTROL STRUCTURES
+        //CONTROL STRUCTURES
 
-            //if(/*condition*/){
-            //execute code if condition is met to be true
+        //if(/*condition*/){
+        //execute code if condition is met to be true
 
-            //        MINI EXERCISE
-            // TODO: create three unique usernames (user1, user2, user3).
-            //  create a fourth user and set it equal to user input. (ask
-            //  the user to enter a username). if the username has already
-            //  been used by user1, user2, or user3, print "sorry this
-            //  username already exists.
+        //        MINI EXERCISE
+        // TODO: create three unique usernames (user1, user2, user3).
+        //  create a fourth user and set it equal to user input. (ask
+        //  the user to enter a username). if the username has already
+        //  been used by user1, user2, or user3, print "sorry this
+        //  username already exists.
 
 //            scanner sc = new Scanner(System.in);
 //
@@ -52,9 +52,39 @@
 //            String user4 = sc.nextLine();
 //            if(user4.equalsIgnoreCase(user1) || user4.equalsIgnoreCase(user2) || user4.equalsIgnoreCase(user3) {
 //                System.out.println("sorry this username already exists");
+
+
 ////            }
 ////
 ////        }
 //
+//    }
+//}
+
+
+    }}
+//TODO: refactor your code from the previous mini exercise into a switch statement
+
+//public class SwitchCaseExample1 {
+//
+//    public static void main(String args[]){
+//        int num=2;
+//        switch(num+2)
+//        {
+//            case 1:
+//                System.out.println("Case1: Value is: "+num);
+//            case 2:
+//                System.out.println("Case2: Value is: "+num);
+//            case 3:
+//                System.out.println("Case3: Value is: "+num);
+//            default:
+//                System.out.println("Default: Value is: "+num);
+
+//ex from web, took screenshot need to add in here the answer to mini exercise.
+
+
+//TODO: using previous mini exercise, allow the user to input a new username several times until a unique username is entered.
+//        }
+
 //    }
 //}
