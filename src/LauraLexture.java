1a260c9f7a2919fb96f322fdd182915eba0ba0a3
tiffannyhadden <tiffanny.hadden@gmail.com>
2021-06-29 18:17:57 -0500

    adds changes

--- src/LauraLexture.java
+++ src/LauraLexture.java
@@ -190,12 +190,9 @@
 //        }
 
 
-//    WARMUP
-//    TODO: prompt a user to tell us about themselves (age, name, if
-//     they have any pets (true/false). Using method overloading, create
-//     methods that will return the first fact they told us about
-//     themselves. ex: I can input: 18 -- Laura -- false in which case I
-//     expect you to return: 18. but if I tell you my name first: Laura --
-//     false -- 18. I expect you to return "Laura" ... etc.
-
+//    TODO: you are a sports manager. Prompt your client who is training
+//     for a marathon how many miles they have to run today. You're the
+//     coach, so you decide how many miles they ran that day. print out
+//     how many miles they have left in training until they have 0 miles
+//     left.
 
