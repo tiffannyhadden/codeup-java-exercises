2c5c0c929b76f0aff484e002ab4e00fbc14371bb
tiffannyhadden <tiffanny.hadden@gmail.com>
2021-07-06 11:28:12 -0500

    adds classes to package

---
+++ src/warmup/Solution.java
@@ -0,0 +1,10 @@
+package warmup;
+
+public class Solution {
+    public static void main(String[] args) {
+        // create an object for each of the classes and call the following methods :
+//        1 - method of parent class by object of parent class
+//        2 - method of child class by object of child class
+//        3 - method of parent class by object of child class
+    }
+}
--- src/warmup/Subclass.java
+++ src/warmup/Subclass.java
@@ -1,4 +1,7 @@
 package warmup;
 
 public class Subclass {
+    public static void main(String[] args) {
+        System.out.println("I'm the Subclass");
+    }
 }
