0d8906fcbdcf0b0f229b86065fcb2a4fbda40372
tiffannyhadden <tiffanny.hadden@gmail.com>
2021-07-08 13:12:48 -0500

    adds changes

--- src/collection/ArrayLecture.java
+++ src/collection/ArrayLecture.java
@@ -5,10 +5,25 @@ import java.util.Arrays;
 public class ArrayLecture {
 
     public static void main(String[] args) {
+ //param fake
+        int fakeParam = 10;
+        int[] intArray = new int[fakeParam];
 
-        int[] intArray = new int[] {1, 2, 3};
 
-        System.out.println(Arrays.toString(intArray)); // "[1, 2, 3]"
+
+        intArray[0] = 1;
+        System.out.println(Arrays.toString(getIntArray(10))); // "[1, 2, 3]"
+
+
+    }
+
+    public static int[] getIntArray(int length){
+
+        int[] intArray = {1, 2, 3 };
+        for(int i = 0; i < intArray.length; i++){
+            intArray[i] = i + 1;
+        }
+        return intArray;
 
 
     }
