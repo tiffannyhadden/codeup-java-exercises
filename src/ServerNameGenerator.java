b84bf036c15566fbc947fe051b9de23b1d6f0b8d
tiffannyhadden <tiffanny.hadden@gmail.com>
2021-07-08 18:53:58 -0500

    adds changes

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,6 +1,7 @@
 public class ServerNameGenerator {
     String[] adjectives = {"greasy", "sharp", "beautiful", "smelly", "charming", "trifflin", "cool", "perfect", "punctual", "tasty"};
     String[] nouns = {"person", "money", "seed", "man", "woman", "card", "bottle", "office", "table", "computer"};
+}
 
 //        public static String getRandomName(String[] inputArr) {
 //            return
