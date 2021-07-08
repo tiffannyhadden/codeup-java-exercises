0615213015e62f9c332248d8f32ef3f06c32b4cc
tiffannyhadden <tiffanny.hadden@gmail.com>
2021-07-08 13:03:47 -0500

    adds array lecture

---
+++ src/collection/ArrayLecture.java
@@ -0,0 +1,15 @@
+package collection;
+
+import java.util.Arrays;
+
+public class ArrayLecture {
+
+    public static void main(String[] args) {
+
+        int[] intArray = new int[] {1, 2, 3};
+
+        System.out.println(Arrays.toString(intArray)); // "[1, 2, 3]"
+
+
+    }
+}
