8cf9f475f113ea475edfde5d049f3b6fa02ad397
tiffannyhadden <tiffanny.hadden@gmail.com>
2021-07-08 19:56:25 -0500

    adds 2

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,10 +1,23 @@
+import shapes.Rectangle;
+
+import java.util.Random;
+
 public class ServerNameGenerator {
-    String[] adjectives = {"greasy", "sharp", "beautiful", "smelly", "charming", "trifflin", "cool", "perfect", "punctual", "tasty"};
-    String[] nouns = {"person", "money", "seed", "man", "woman", "card", "bottle", "office", "table", "computer"};
+   private static String[] adjectives = {"greasy", "sharp", "beautiful", "smelly", "charming", "trifflin", "cool", "perfect", "punctual", "tasty"};
+   private static  String[] nouns = {"person", "money", "seed", "man", "woman", "card", "bottle", "office", "table", "computer"};
+
+
+    public static String getRandomName(String[] inputArr) {
+        Random randomNum = new Random();
+        int idx = randomNum.nextInt(inputArr.length);
+
+        return inputArr[idx];
+    }
+
+    public static void main(String[] args) {
+        System.out.printf("Here is your server name:\n%s-%s",getRandomName(adjectives), getRandomName(nouns));
+    }
 }
 
-//        public static String getRandomName(String[] inputArr) {
-//            return
-//        }
-//
-//}
+
+
