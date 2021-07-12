0e4de291130b239d381c424b099c1902e683dca3
tiffannyhadden <tiffanny.hadden@gmail.com>
2021-07-12 00:51:18 -0500

    adds changes

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -1,5 +1,24 @@
 package grades;
 
+import java.util.HashMap;
+
+
 public class GradesApplication {
 
+    public static void main(String[] args) {
+
+        Student tiff = new Student("tiff");
+        tiff.addGrade(78);
+        tiff.addGrade(45);
+        tiff.addGrade(89);
+        tiff.addGrade(90);
+
+        HashMap<String, Student> students = new HashMap<String, Student>();
+
+        students.put("tiffy@github.com", tiff);
+
+        for (String student: students.keySet())
+        System.out.println(student);
+
+    }
 }
--- src/grades/Student.java
+++ src/grades/Student.java
@@ -15,7 +15,7 @@ public class Student {
 
     public static void main(String[] args) {
 
-        Student student = new Student("Amado");
+        Student student = new Student("Tiff");
 
 
         System.out.println(student.getName());
