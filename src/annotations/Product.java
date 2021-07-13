9812f26b077ce1c37782535d23b7b0bde0d9990a
tiffannyhadden <tiffanny.hadden@gmail.com>
2021-07-13 12:50:28 -0500

    adds colors class

--- src/annotations/Product.java
+++ src/annotations/Product.java
@@ -1,18 +1,18 @@
-package annotations;
-
-@NameofAnnotation // syntax // above the thing that your'e applying it to
-//no semi-colons
-@Entity
-public class Product {
-    //define property name
-    @Persistent
-    protected String name = null;
-
-    public String getName(){
-        return name;
-    }
-
-    public void setName(@Optional name){
-        this.name = name;
-    }
-}
+//package annotations;
+//
+//@NameofAnnotation // syntax // above the thing that your'e applying it to
+////no semi-colons
+//@Entity
+//public class Product {
+//    //define property name
+//    @Persistent
+//    protected String name = null;
+//
+//    public String getName(){
+//        return name;
+//    }
+//
+//    public void setName(@Optional name){
+//        this.name = name;
+//    }
+//}
