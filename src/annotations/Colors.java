0d90ceafcec1bac9847d3aa1cec2b1d0cfdd7f0c
tiffannyhadden <tiffanny.hadden@gmail.com>
2021-07-13 13:08:10 -0500

    adds changes

--- src/annotations/Colors.java
+++ src/annotations/Colors.java
@@ -8,6 +8,12 @@ class Red{
 }
 
 class Orange extends Red{
+    //@Override
+    // comments the compiler // it checks to see if that
+    //comment is doing what it is supposed to do
+    // reason why you want to add @Override acts like a safety net
+    // hey this method should be overriding the method in its parents
+    // class of red
      public void show(){
          System.out.println("I am orange");
      }
